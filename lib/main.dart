2bf10d9b072accaa6a8fe60b77fa565929ea9f07
abdallasayed <abdallasayed190@gmail.com>
2026-03-09 22:31:33 +0200

    Initial structure for Love Space App

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Scaffold(
+  appBar: AppBar(title: Text("ذكرياتنا والمواضيع")),
+  body: Center(child: Text("قيد التطوير يا زميلي!")),
+)));
